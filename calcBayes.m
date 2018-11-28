018e296b7eb2ec8f512b5bab4d1c914c061d6b3e
vbadri <vbadri@vbadri-ss.innovation.dell.com>
2018-11-27 23:29:01 -0800

    Differentiate model and actual threshold

--- calcBayes.m
+++ calcBayes.m
@@ -3,11 +3,19 @@
 
 function calcBayes(loopcount)
 
-th1 = 0.9 
-th2 = 0.7 
+mth1 = 0.9 
+mth2 = 0.9 
+mth3 = 0.8 
+mth4 = 0.8 
+mth5 = 0.2 
+
+
+
+th1 = 0.8 
+th2 = 0.9 
 th3 = 0.7 
-th4 = 0.9 
-th5 = 0.3 
+th4 = 0.8 
+th5 = 0.1 
 NUM = 600
 
 pt1 = 0; 
@@ -15,12 +23,13 @@ pt2 = 0;
 pt3 = 0;
 pt4 = 0;
 pt5 = 0; 
+psdrop = 0;
 
 
   
 %% In a loop Geta bunch of random data,  Caluclate our probailities , Keep updating 
 for loop = 1:loopcount 
-  [drop input] = switchmodel(th1,th2,th3,th4,th5,NUM,1);
+  [drop input] = switchmodel(mth1,mth2,mth3,mth4,mth5,NUM,1);
   % normalize the drop array 
   sessiondrop = ( drop > 0);
   t1  = (input(:,1) > th1);
@@ -34,6 +43,7 @@ for loop = 1:loopcount
   t3count = sum(t3);
   t4count = sum(t4);
   t5count = sum(t5);
+  psdrop = psdrop + dropcount/NUM;
   pt1 = (pt1 + t1count/dropcount);
   pt2 = (pt2 + t2count/dropcount);
   pt3 = (pt3 + t3count/dropcount);
@@ -46,7 +56,7 @@ pt2
 pt3
 pt4
 pt5
-
+psdrop
 
   
 end
